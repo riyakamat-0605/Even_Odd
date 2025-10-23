9907ce91b85e96bddcd77d96a04509e98aa73524
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 14:32:15 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
