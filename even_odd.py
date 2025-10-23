1fd33220ef9963a8f4102f6c017cfaafdf77c435
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 14:37:29 +0530

    save to prime-count

--- even_odd.py
+++ even_odd.py
@@ -1,13 +1,17 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-even_count = 0
-odd_count = 0
-
-for num in numbers:
-    if num % 2 == 0:
-        even_count += 1
-    else:
-        odd_count += 1
+even_count = sum(1 for n in numbers if n % 2 == 0)
+odd_count = sum(1 for n in numbers if n % 2 != 0)
+prime_count = sum(1 for n in numbers if is_prime(n))
 
 print("Even numbers:", even_count)
 print("Odd numbers:", odd_count)
+print("Prime numbers:", prime_count)
